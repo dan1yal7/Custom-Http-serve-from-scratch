7aec9e71625e465d45d1cd23383957b9d371200e
Daniyal <daniiltemirkhanov@gmail.com>
2025-10-03 04:58:00 +0500

    tcp client on socket (not refactored version)

---
+++ HttpServerCustom/HttpServerCustom/ClientSide.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+var port = 80;
+var url = "www.google.com";
+
+using Socket tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+try
+{
+    await tcpSocket.ConnectAsync(url, port);
+    var message = $"GET / HTTP/1.1\r\nHost: {url}\r\nConnection: close\r\n\r\n";
+    var messageBytes = Encoding.UTF8.GetBytes(message);
+    await tcpSocket.SendAsync(messageBytes);
+
+    tcpSocket.Shutdown(SocketShutdown.Send);
+
+    var responseBytes = new byte[512];
+
+    var builder = new StringBuilder();
+    int bytes;
+    do
+    {
+        bytes = await tcpSocket.ReceiveAsync(responseBytes);
+        string response = Encoding.UTF8.GetString(responseBytes, 0, bytes);
+        builder.Append(response);
+    }
+    while (bytes > 0);
+    Console.WriteLine(builder);
+
+}
+catch(SocketException ex)
+{
+    Console.WriteLine($"{ex.Message}");
+}
