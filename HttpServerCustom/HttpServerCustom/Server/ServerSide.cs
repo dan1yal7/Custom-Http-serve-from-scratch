c4e6ff6d758dcb8654216773390e4053b8961188
Daniyal <daniiltemirkhanov@gmail.com>
2025-10-03 05:33:02 +0500

    Tcp server on socket

--- HttpServerCustom/HttpServerCustom/Server/ServerSide.cs
+++ HttpServerCustom/HttpServerCustom/Server/ServerSide.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Sockets;
 
 namespace HttpServerCustom.Server
 {
     public class ServerSide
     {
-
+       IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Any, 80);
     }
 }
