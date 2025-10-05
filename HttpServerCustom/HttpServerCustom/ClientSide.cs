ca7fa680303ad4f84c24c41650bfc3aabac1a9bf
Daniyal <daniiltemirkhanov@gmail.com>
2025-10-05 23:57:04 +0500

    refactoring of client side

--- HttpServerCustom/HttpServerCustom/ClientSide.cs
+++ HttpServerCustom/HttpServerCustom/ClientSide.cs
@@ -1,6 +1,7 @@
 ﻿using HttpServerCustom.Server;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading.Channels;
 
 
 ServerSide serverSide = new ServerSide();
@@ -8,32 +9,48 @@ serverSide.TcpServerFunction();
 
 var port = 8888;
 var url = "127.0.0.1";
+var response = await SocketSendRecieve(url, port);
 
-using Socket tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-try
+
+async Task<Socket?> ConnectSocketAsync(url, port)
 {
-    await tcpSocket.ConnectAsync(url, port);
+  Socket tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+    try
+    {
+        await tcpSocket.ConnectAsync(url, port);
+        return tcpSocket;
+    }
+    catch(SocketException ex)
+    {
+        Console.WriteLine(ex.Message);
+        tcpSocket.Close();
+    }
+    return null;
+}
+
+async Task<string> SocketSendRecieve(string url, int port)
+{
+    using Socket? socket = await ConnectSocketAsync(url, port);
+    if (socket is null)
+    {
+        return $"Connection could not be established with {url}";
+    }
     var message = $"GET / HTTP/1.1\r\nHost: {url}\r\nConnection: close\r\n\r\n";
     var messageBytes = Encoding.UTF8.GetBytes(message);
-    await tcpSocket.SendAsync(messageBytes);
+    await socket.SendAsync(messageBytes);
 
-    tcpSocket.Shutdown(SocketShutdown.Send);
-
+    socket.Shutdown(SocketShutdown.Send);
     var responseBytes = new byte[512];
 
     var builder = new StringBuilder();
     int bytes;
     do
     {
-        bytes = await tcpSocket.ReceiveAsync(responseBytes);
+        bytes = await socket.ReceiveAsync(responseBytes);
         string response = Encoding.UTF8.GetString(responseBytes, 0, bytes);
         builder.Append(response);
     }
     while (bytes > 0);
-    Console.WriteLine(builder);
+    return builder.ToString();
 
-}
-catch(SocketException ex)
-{
-    Console.WriteLine($"{ex.Message}");
 }
