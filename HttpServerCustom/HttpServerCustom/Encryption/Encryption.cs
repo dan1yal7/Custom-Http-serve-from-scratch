2b37c66510bf8dd488d5a4c91b6561761fc4c5fe
Daniyal <daniiltemirkhanov@gmail.com>
2025-10-10 22:39:29 +0500

    class with further implementation of secure communication

---
+++ HttpServerCustom/HttpServerCustom/Encryption/Encryption.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpServerCustom.Encryption
+{
+    internal class Encryption
+    {
+    }
+}
