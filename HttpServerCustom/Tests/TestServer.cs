2c5c92f2cced302c0bb43d663c69bb637f0ae119
Daniyal <daniiltemirkhanov@gmail.com>
2025-10-17 01:35:05 +0500

    refac with naming

--- HttpServerCustom/Tests/TestServer.cs
+++ HttpServerCustom/Tests/TestServer.cs
@@ -1,7 +1,19 @@
-﻿namespace Tests
+﻿using HttpServerCustom.Server;
+using Moq;
+using System.Net.Sockets;
+using Xunit;
+
+namespace Tests
 {
     public class TestServer
     {
+        [Fact]
+        public async Task TcpServerFunc()
+        {
+            //Arrange 
+            var mock = new Mock<Socket>();
+            var clientSide = new ServerSide();
 
+        }
     }
 }
