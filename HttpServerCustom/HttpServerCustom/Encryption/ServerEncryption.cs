29358001fda2e5429d853cd76abbffc4c357a2f2
Daniyal <daniiltemirkhanov@gmail.com>
2025-11-10 23:52:36 +0500

    servcertificate

--- HttpServerCustom/HttpServerCustom/Encryption/ServerEncryption.cs
+++ HttpServerCustom/HttpServerCustom/Encryption/ServerEncryption.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using HttpServerCustom.Server;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HttpServerCustom.Encryption
 {
-    public class ServerEncryption
+    public   class ServerEncryption
     {
+        private readonly ServerSide _serverside; 
+        public ServerEncryption(ServerSide serverside)
+        {
+            _serverside = serverside;
+        }
+        static X509Certificate servercertificate = null; 
+
+        public static void RunServer(string certificate)
+        {
+            servercertificate = X509Certificate.CreateFromCertFile(certificate);
+
+        }
     }
 }
