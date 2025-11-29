159382dacd78c96adbbdf9a3bac189becaab621a
Daniyal <daniiltemirkhanov@gmail.com>
2025-11-29 05:32:00 +0500

    ClientEncryption ServerEncryption were deleted

--- HttpServerCustom/HttpServerCustom/Program.cs
+++ HttpServerCustom/HttpServerCustom/Program.cs
@@ -1,17 +1,13 @@
 ﻿using HttpServerCustom.Client;
-using HttpServerCustom.Encryption;
 using HttpServerCustom.Server;
 using System.Net.Sockets;
 
 var server = new ServerSide();
 _ = Task.Run(server.TcpServerFunction);
 
-//ServerEncryption serverEncryption = new ServerEncryption();
-
 await Task.Delay(1000);
 
 var client = new ClientSide();
-
 var port = 8888;
 var url = "127.0.0.1";
 string response = await client.SocketSendRecieve(url, port);
